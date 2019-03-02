d9b63fe3d22ade5abc1dd6ff8c35ae42b4de9171
kbmb1236 <kbrit804@gmail.com>
2019-03-02 17:26:49 +0000

    i did it... well at least i think that i did

--- problemSetDay4.py
+++ problemSetDay4.py
@@ -1,1 +1,10 @@
 # file name: problemSetDay4.py
+
+count = 1
+while count <= 100:
+    if count % 5==0:
+        print("buzz")
+    elif count % 5==0:
+        print("buzz")
+    elif count % 3==0:
+        print("fizz")
